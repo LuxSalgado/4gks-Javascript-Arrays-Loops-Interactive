a5f46ff1be148b46ca65a889a725bb7e459df9d2
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-07-21 04:32:57 +0000

    Ejercicios resueltos hasta el número 7.3

--- exercises/07.3-Flip-Array/app.js
+++ exercises/07.3-Flip-Array/app.js
@@ -2,7 +2,7 @@ var arr = [45,67,87,23,5,32,60];
 
 //Your code here.
 let auxArray = [];
-for (let i=arr.length; i>0; i--){
-    auxArray.push(arr[i-1]);
+for (let i=arr.length-1; i>=0; i--){
+    auxArray.push(arr[i]);
 }
 console.log(auxArray);
