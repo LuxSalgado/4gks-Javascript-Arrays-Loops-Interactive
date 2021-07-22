9e193fb898929ec582668037da40b80716d07efa
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-07-22 23:25:43 +0000

    Ejercicios resueltos hasta el número 20.4

--- exercises/20.6-Yes-and-no/app.js
+++ exercises/20.6-Yes-and-no/app.js
@@ -1,3 +1,13 @@
 let theBools = [0,1,0,0,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1];
 
 //your code here
+
+var myFunction = function(dato){
+    if (dato == 0){
+        return "woko";
+    } else if (dato ==1) {
+        return "wiki"
+    }
+}
+ var newArray = theBools.map(myFunction);
+ console.log(newArray);
